b20791faa949973ce2c0c821137719d03a76de1a
taufeeq-mustafa <158837644+taufeeq-mustafa@users.noreply.github.com>
2024-02-04 21:48:02 +0500

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,18 @@
+
+package project;
+
+
+public class Project {
+
+    
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new welcome().setVisible(true);
+            }
+        });
+         
+        
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Criminal-Record-Management-System
+# Criminal-Management-System
+This repository contains source code for project that was created by Taufeeq Mustafa for SC&amp;D lab project
